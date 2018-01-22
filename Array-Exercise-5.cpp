5d8fae121b40d8114cff52b77aad8d5a9a13e43f
KillerBee3012 <33883984+KillerBee3012@users.noreply.github.com>
2018-01-22 17:15:48 +0800

    Update Array-Exercise-5

--- Array-Exercise-5.cpp
+++ Array-Exercise-5.cpp
@@ -1,19 +1,29 @@
-//using the random function, the program make it own random number and find the maximum random number and print it out.
-//finish this program up
-//thank you.
+//NAME : AIDIL SUFFIAN BIN ABU BAKAR
+//STUDENT ID : A17DW2253
 
 #include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
 int main()
 {
-	i, max=0;
-	list[100];
+	int i, max = 100, holder = 0;
+	int list[100];
 
 	//initialize the array with random values
-	for(/*i<100*/) list[i] = /*insert the random function here*/;
+	for(i = 0; i < max; i++ /*i<100*/) {
+		list[i] = rand();
+		cout << list[i] << endl;	
+	}
+	
 
-	//find the maximum val
-	for(/*i>100*/)
-		if(max<list[i]) max=list[i];
-
-	cout<<"Maximum value: "<<max;
+	//find the maximum val * comparing holder
+	for(i = 0; i < max; i++ /*i>100*/) {
+		if (list[i] >= holder) {
+			holder = list[i];
+		}
+	}
+	
+	cout << endl << "Maximum value is = " << holder;
 }
