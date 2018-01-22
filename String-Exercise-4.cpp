09f314868ad6355dcbc2831f652b66d4102a07b1
KillerBee3012 <33883984+KillerBee3012@users.noreply.github.com>
2018-01-22 17:26:21 +0800

    Update String-Exercise-4

--- String-Exercise-4.cpp
+++ String-Exercise-4.cpp
@@ -1,20 +1,21 @@
-//convert string to uppercase
-//complete the fragment code below using the string to uppercase function.
+//Name : AIDIL SUFFIAN BIN ABU BAKAR
+//STUDENT ID :A17DW2253
 
 #include <iostream>
+#include <string.h>
+
+using namespace std;
 
 int main()
 {
 	char lowerToUpper[80];
 
-	/*use string copy function here*/(lowerToUpper, "This is a check");
+	/*use string copy function here*/strcpy(lowerToUpper, "This is a check");
 
-	for(i=0;i<80;i++)
+	for(int i=0;i<80;i++)
 	{
-		lowerToUpper[i] = /*uppercase function*/(lowerToUpper[i]);
-
+		lowerToUpper[i] = toupper(lowerToUpper[i]);
 	}
 
 	cout<<lowerToUpper<<endl;
 }
-	
